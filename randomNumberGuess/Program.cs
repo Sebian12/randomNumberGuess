b44614a6b3704078b0d51fae86bf27ccc07322e8
Sebian12 <256605829+Sebian12@users.noreply.github.com>
2026-02-17 21:18:22 +0100

    Added randomizer

--- randomNumberGuess/Program.cs
+++ randomNumberGuess/Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Random randomizer = new Random();
+int maxValue = 1000;
+int targetNumber = randomizer.Next(maxValue + 1); // Generating a random number between 0 and 1000
+
+Console.WriteLine("Welcome to Random Number Guess!");
+Console.WriteLine($"I have chosen a number between 0 and {maxValue}. Try to guess it!");
